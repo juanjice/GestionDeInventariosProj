4647640669d4d0c87c65c6f29951fd815f6e884c
juanjice <48636434+juanjice@users.noreply.github.com>
2020-10-09 15:26:23 -0500

    cree el intent de la interfaz del login

---
+++ app/src/main/java/com/example/homecenterapp/ui/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.homecenterapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.homecenterapp.R.layout.activity_login
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_login)
+    }
+}
--- app/src/main/java/com/example/homecenterapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/homecenterapp/ui/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.example.homecenterapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.homecenterapp.R.layout.activity_main
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.homecenterapp.R
 
-class MainActivity : AppCompatActivity() {
+import com.example.homecenterapp.R.layout.activity_main
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_main)
+        val loginBtn : Button =findViewById(R.id.login_button)
+        loginBtn.setOnClickListener(this)
+    }
 
+    override fun onClick(v: View?) {
+        Log.i("APP","el usuario a entrado al login")
+        val intent = Intent(this,LoginActivity::class.java)
+        startActivity(intent)
     }
 
 
 }
+
+
